af0387796a3575806dc998e52c3dbd1a100c2406
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-08-17 05:45:35 -0500

    Literal Testing Template

--- Testing/__test__/src_test/email_validator_test.test.js
+++ Testing/__test__/src_test/email_validator_test.test.js
@@ -3,17 +3,35 @@ import validator from '../../src/email_validator';
 
 describe('Testing Email Validation', () => {
 
-    describe.only.each([
-        ['africanKing@gmail.com', true],
-        ['acompletenoobsmoke@ngc.com', true],
-        ['africankinggmail.com', false],
-        ['acompletenoobsmoke@ngc', false]
-    ])('email %s should be %o', (email, expected) => {
-        test(`when ${email} return ${expected}`, () => {
-            const actualResult = validator(email);
-            expect(actualResult).toBe(expected);
+    describe.only('Utilizing Table Testing', () =>{
+        describe.each([
+            ['africanKing@gmail.com', true],
+            ['acompletenoobsmoke@ngc.com', true],
+            ['africankinggmail.com', false],
+            ['acompletenoobsmoke@ngc', false]
+        ])('email %s should be %o', (email, expected) => {
+            test(`when ${email} return ${expected}`, () => {
+                const actualResult = validator(email);
+                expect(actualResult).toBe(expected);
+            })
         })
+
+        describe.each`
+        email                           | expected
+        ${'africanKing@gmail.com'}      | ${true}
+        ${'acompletenoobsmoke@ngc.com'} | ${true}
+        ${'africankinggmail.com'}       | ${false}
+        ${'acompletenoobsmoke@ngc'}     | ${false}
+        `('email $email should return $expected', ({email, expected}) => {
+            test(`(Literal Template Version)\nwhen ${email} return ${expected}`, () => {
+                const actualResult = validator(email);
+                expect(actualResult).toBe(expected);
+            })
+        })
+
     })
+
+   
     
     test('True when email is valid', () => {
         let email = 'africanKing@gmail.com';

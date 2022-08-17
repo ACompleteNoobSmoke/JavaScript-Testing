7ef053208a9c2295d9767a0ac6e09123f3cee3ab
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-08-17 05:26:28 -0500

    Utilized the each functionality for validation testing

--- Testing/__test__/src_test/email_validator_test.test.js
+++ Testing/__test__/src_test/email_validator_test.test.js
@@ -2,6 +2,18 @@ import validator from '../../src/email_validator';
 
 
 describe('Testing Email Validation', () => {
+
+    describe.only.each([
+        ['africanKing@gmail.com', true],
+        ['acompletenoobsmoke@ngc.com', true],
+        ['africankinggmail.com', false],
+        ['acompletenoobsmoke@ngc', false]
+    ])('email %s should be %o', (email, expected) => {
+        test(`when ${email} return ${expected}`, () => {
+            const actualResult = validator(email);
+            expect(actualResult).toBe(expected);
+        })
+    })
     
     test('True when email is valid', () => {
         let email = 'africanKing@gmail.com';

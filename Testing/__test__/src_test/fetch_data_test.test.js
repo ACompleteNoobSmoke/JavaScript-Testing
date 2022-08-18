2354e9b184b454008194cb73ed14e91db44a560d
ACompleteNoobSmoke <OmoruyiOmofonmwan@yahoo.com>
2022-08-18 05:03:09 -0500

    Test Async Functions

--- Testing/__test__/src_test/fetch_data_test.test.js
+++ Testing/__test__/src_test/fetch_data_test.test.js
@@ -9,11 +9,22 @@ describe('Testing Promise', () => {
         return expect(fetchData()).resolves.toBe('data back from the server')
     })
 
-    test.only('Testing if promise rejects', () => {
+    test('Testing if promise resolves async', async () => {
+        // const data = await fetchData();
+        // expect(data).toBe('data back from the server')
+
+        await expect(fetchData()).resolves.toBe('data back from the server');
+    })
+
+    test.skip('Testing if promise rejects', () => {
         // return fetchData().catch(e => {
         //     expect(e).toBe('oops error');
         // })
 
         return expect(fetchData()).rejects.toBe('oops error');
+    })
+
+    test.skip('Testing if promise rejects async', async () => {
+        await expect(fetchData).rejects.toMatch('oops error');
     })
 })
